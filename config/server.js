84e58ffbcbb7aca6258deb0e06ecfc0198bbc374
Renato Oliveira <oliversieto@gmail.com>
2021-02-16 22:16:18 -0400

    envio inicial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3aa6a02f746d44aef528990585e879c1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '71097a30-dc5b-47e4-af79-9abe20a22c3f'
+};
